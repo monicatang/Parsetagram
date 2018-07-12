537f328a7c9f2551b1d9cc36f86f740cdd359d4e
monicatang <monicaa.tang@gmail.com>
2018-07-12 14:11:46 -0700

    Feed loads in a fragment

---
+++ app/src/main/java/me/monicatang/parsetagram/FeedFragment.java
@@ -0,0 +1,98 @@
+package me.monicatang.parsetagram;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import me.monicatang.parsetagram.model.Post;
+
+public class FeedFragment extends Fragment {
+
+    ArrayList<Post> posts;
+    PostAdapter postAdapter;
+
+    // Views
+    @BindView(R.id.rvFeed) RecyclerView rvFeed;
+    @BindView(R.id.swipeContainer) SwipeRefreshLayout swipeContainer;
+
+    // The onCreateView method is called when Fragment should create its View object hierarchy,
+    // either dynamically or via XML layout inflation.
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        // Defines the xml file for the fragment
+        return inflater.inflate(R.layout.fragment_feed, parent, false);
+    }
+
+    // This event is triggered soon after onCreateView().
+    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Setup any handles to view objects here
+        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+
+        // resolve view lookups
+        ButterKnife.bind(this, view);
+
+        // init Array List (data source)
+        posts = new ArrayList<>();
+        // construct adapter from data
+        postAdapter = new PostAdapter(posts);
+        // recyclerView setup (layout manager, use adapter)
+        rvFeed.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rvFeed.setAdapter(postAdapter);
+
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                postAdapter.clear();
+                loadTopPosts();
+                swipeContainer.setRefreshing(false);
+            }
+        });
+
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+        loadTopPosts();
+
+    }
+
+    private void loadTopPosts() {
+        final Post.Query postsQuery = new Post.Query();
+        postsQuery.getTop().withUser();
+
+        postsQuery.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> objects, ParseException e) {
+                if (e == null) {
+                    for (int i = 0; i < objects.size(); ++i) {
+                        Log.d("HomeActivity","Post[" + i + "] = "
+                                + objects.get(i).getDescription()
+                                + "\nusername =" + objects.get(i).getUser().getUsername());
+                    }
+                    postAdapter.addAll(objects);
+                    postAdapter.notifyDataSetChanged();
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
--- app/src/main/java/me/monicatang/parsetagram/HomeActivity.java
+++ app/src/main/java/me/monicatang/parsetagram/HomeActivity.java
@@ -4,33 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
-
-import com.parse.FindCallback;
-import com.parse.ParseException;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import me.monicatang.parsetagram.model.Post;
 
 
 public class HomeActivity extends AppCompatActivity {
 
-    ArrayList<Post> posts;
-    PostAdapter postAdapter;
-
-    // Views
-    @BindView(R.id.rvFeed) RecyclerView rvFeed;
-    @BindView(R.id.swipeContainer) SwipeRefreshLayout swipeContainer;
     @BindView(R.id.toolbar) Toolbar toolbar;
     @BindView(R.id.bottom_navigation) BottomNavigationView bottomNavigationView;
 
@@ -45,28 +29,6 @@ public class HomeActivity extends AppCompatActivity {
         // set action bar
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-
-        // init Array List (data source)
-        posts = new ArrayList<>();
-        // construct adapter from data shtulvjrelrkvklbnhjfugtcrrhrtrcveource
-        postAdapter = new PostAdapter(posts);
-        // recyclerView setup (layout manager, use adapter)
-        rvFeed.setLayoutManager(new LinearLayoutManager(this));
-        rvFeed.setAdapter(postAdapter);
-
-        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                postAdapter.clear();
-                loadTopPosts();
-                swipeContainer.setRefreshing(false);
-            }
-        });
-        // Configure the refreshing colors
-        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
-                android.R.color.holo_green_light,
-                android.R.color.holo_orange_light,
-                android.R.color.holo_red_light);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(
                 new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -88,39 +50,13 @@ public class HomeActivity extends AppCompatActivity {
                     }
                 });
 
-        loadTopPosts();
+        // Begin the transaction
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        // Replace the contents of the container with the new fragment
+        ft.replace(R.id.feed, new FeedFragment());
+        // or ft.add(R.id.your_placeholder, new FooFragment());
+        // Complete the changes added above
+        ft.commit();
     }
 
-//    private void logOut() {
-//        ParseUser.logOut();
-//        Intent intent = new Intent(this, LoginActivity.class);
-//        startActivity(intent);
-//        finish();
-//        if(ParseUser.getCurrentUser() == null) {
-//            Log.i("Logout", "Log out successful"); // this will now be null
-//        }
-//
-//    }
-
-    private void loadTopPosts() {
-        final Post.Query postsQuery = new Post.Query();
-        postsQuery.getTop().withUser();
-
-        postsQuery.findInBackground(new FindCallback<Post>() {
-            @Override
-            public void done(List<Post> objects, ParseException e) {
-                if (e == null) {
-                    for (int i = 0; i < objects.size(); ++i) {
-                        Log.d("HomeActivity","Post[" + i + "] = "
-                                + objects.get(i).getDescription()
-                                + "\nusername =" + objects.get(i).getUser().getUsername());
-                    }
-                    postAdapter.addAll(objects);
-                    postAdapter.notifyDataSetChanged();
-                } else {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
 }
